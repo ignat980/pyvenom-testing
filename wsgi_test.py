86b9422b6f722a5ed8d09e6d64d558c97f7731b1
Ignat Remizov <ignat980@gmail.com>
2016-04-21 14:48:18 -0700

    Added the code to test a google app engine app using webtest

--- wsgi_test.py
+++ wsgi_test.py
@@ -1,6 +1,30 @@
-from wsgiref.simple_server import make_server
-from sample_app import application
+import os
+import sys
 
-httpd = make_server('', 8000, application)
-print "Serving on port 8000..."
-httpd.handle_request()
+sdk_path = '/usr/local/google_appengine/'
+if os.path.exists(os.path.join(sdk_path, 'platform/google_appengine')):
+    sys.path.insert(0, os.path.join(sdk_path, 'platform/google_appengine'))
+else:
+    sys.path.insert(0, sdk_path)
+import dev_appserver
+dev_appserver.fix_sys_path()
+
+try:
+    import appengine_config
+    (appengine_config)
+except ImportError:
+    print "Note: unable to import appengine_config."
+
+from webtest import TestApp
+# from app import app as application  # venom app
+from sample_app import application
+app = TestApp(application)
+
+resp = app.get('/api/v2/helloworld')
+
+print(resp)
+print(resp.request)
+print '{'
+for k, v in resp.request.environ.items():
+    print '    \'{}\': {}'.format(k, v)
+print '}'
